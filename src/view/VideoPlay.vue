b8f04565cc6f094e19c8e637fef8c01c8c9052e9
zhf8 <1715005995@qq.com>
2024-01-18 10:53:58 +0800

    <fix>:播放器样式优化

--- src/view/VideoPlay.vue
+++ src/view/VideoPlay.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="dplayer" style="width: 100%;height: 100%"></div>
+  <div id="dplayer" style="width: 100%;height: 100vh"></div>
 </template>
 <script setup>
 import DPlayer from 'dplayer';
